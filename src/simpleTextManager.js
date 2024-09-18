869bed4e6869dc271f2af0e5e3a0fadee4ff299d
JACK <geco97@gmail.com>
2024-09-18 06:51:49 +0200

    First

---
+++ src/simpleTextManager.js
@@ -0,0 +1,86 @@
+var mongoose = require('mongoose');
+var dbServer = '127.0.0.1:27017';
+const dbPort = '27017';
+const dbName = 'textStore';
+var Text = require('./textStore');
+
+const CHUNKSIZE = 1000; // number of lines per chunk
+
+class SimpleTextManager {    
+    constructor() {
+    }
+
+    connect() {
+				console.log("Attempting to connect to Database...");
+        if (process.env.TEXTSTORE_RS) {
+            console.log('This is a replicaset:', process.env.TEXTSTORE_RS);
+            let connection = process.env.TEXTSTORE_RS
+            let user=process.env.TEXTSTORE_USER || 'noUserSet';
+            let pw=process.env.TEXTSTORE_PW || 'noPasswordSet';
+            let authsource=process.env.MONGO_AUTHSOURCE || 'noAuthSourceSet';
+            
+            return mongoose.connect(connection, {user: user,
+                                                 pass: pw,
+                                                 authSource: authsource})
+                .then( () => console.log('Connected to database', dbName)) // ... This is now hardcoded into the URL for reasons.
+                .catch( (err) => {
+                    console.error('Database connection error', dbName);
+                    console.error(' trying to connect to server:', connection);
+                    console.error(' user:', user, 'password:', pw, 'authsource:', authsource); // TODO: Set these in yaml-file.
+                    console.error('Error message is:', err);
+                    //throw new Error('Failed to connect to database.');
+                });
+        }
+
+        if (!process.env.TEXTSTORE_HOST) {
+            console.log('WARNING: the environment variable TEXTSTORE_HOST is not set');        
+        } else {
+            dbServer = process.env.TEXTSTORE_HOST + ':' + dbPort;
+        }
+
+				let user ="";
+				if (process.env.MONGO_USER) {
+						user= process.env.MONGO_USER + ":" + process.env.MONGO_PW + "@";
+				}
+
+        let connection = `mongodb://${user}${dbServer}/${dbName}`
+				console.log("Using connection", connection);
+        return mongoose.connect(connection)
+            .then( () => console.log('Connected to database', dbName))
+            .catch( (err) => {
+                console.error('Database connection error', dbName);
+                console.error(' trying to connect to server:', connection);
+                console.error('Error message is:', err);
+                //throw new Error('Failed to connect to database.');
+            });
+    }
+
+    simpleSearch(searchString) {
+        console.log("Searching for:", searchString);
+        return Text.find({"contents": {"$regex": searchString,
+                                       "$options": "i"}});
+    }
+
+    addText(title, contents) {
+        contents = contents || '';
+        console.log('Storing text to database');
+        let lines = contents.split(/\n/);
+        let firstLine = 0;
+        let lastLine = firstLine + CHUNKSIZE;
+        while (firstLine <= lines.length) {
+            let chunk = lines.slice(firstLine, lastLine+1).join('\n');
+            firstLine=lastLine+1;
+            lastLine=firstLine + CHUNKSIZE;
+            new Text({name: title, startLine: firstLine, contents: chunk})
+                .save()
+                .catch(err => console.log('Error while inserting test text:', err.message));
+        }
+    }
+    
+    listTexts() {
+        console.log('Retrieving available text titles...');
+        return Text.distinct('name');
+    }
+}
+
+module.exports = SimpleTextManager;
